3e4be739d2ffc29aa3aeef271fa9acb1023740e2
DrInnovatio <turing1723@gmail.com>
2020-03-18 16:08:58 +1000

    Updates

---
+++ precache-manifest.9b8e1961cefd04cf981ba83008597b21.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6c5c820dfec75165c6ace68ab530e8",
+    "url": "/brandon_park/index.html"
+  },
+  {
+    "revision": "656ae0692c3bd67e0f34",
+    "url": "/brandon_park/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "b9160b840a3ebcbab9bd",
+    "url": "/brandon_park/static/css/main.a705fea3.chunk.css"
+  },
+  {
+    "revision": "656ae0692c3bd67e0f34",
+    "url": "/brandon_park/static/js/2.dddd5968.chunk.js"
+  },
+  {
+    "revision": "4251877289940887c25203555ac877f3",
+    "url": "/brandon_park/static/js/2.dddd5968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9160b840a3ebcbab9bd",
+    "url": "/brandon_park/static/js/main.c4b9f713.chunk.js"
+  },
+  {
+    "revision": "36badaee9a042f3cabcb",
+    "url": "/brandon_park/static/js/runtime-main.98226fdc.js"
+  },
+  {
+    "revision": "cdc8415d10c512f6f47b12dc320efef9",
+    "url": "/brandon_park/static/media/JW.cdc8415d.png"
+  },
+  {
+    "revision": "b0fa0ec06bfdea5e7c4d2ac53be99166",
+    "url": "/brandon_park/static/media/JW2.b0fa0ec0.jpg"
+  },
+  {
+    "revision": "9797706d9cb1a5e7780fa1471dffc571",
+    "url": "/brandon_park/static/media/NewRelic.9797706d.png"
+  },
+  {
+    "revision": "c0844bc79d3cc71a9e35e2bb89b24cf1",
+    "url": "/brandon_park/static/media/crowd.c0844bc7.png"
+  },
+  {
+    "revision": "97b5145f5fddbc3575a625e570d23da5",
+    "url": "/brandon_park/static/media/newY.97b5145f.png"
+  },
+  {
+    "revision": "454e532fb091e41f64b34b09e8875174",
+    "url": "/brandon_park/static/media/pound.454e532f.png"
+  }
+]);
